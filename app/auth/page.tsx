09eccef77a32772fa01eb16fc2760fc2a2ba6854
amrelskhawy <amrelskhawy@gmail.com>
2024-02-14 23:22:44 +0200

    first commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,10 @@
+export default function Auth() {
+  return (
+    <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover bg-center">
+      <div className="h-full w-full bg-black lg:opacity-50">
+        
+
+      </div>
+    </div>
+  )
+}
