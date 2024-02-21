b644a30b19f98e3ec2f26d769abbaf28e25383f0
amrelskhawy <amrelskhawy@gmail.com>
2024-02-21 21:41:02 +0200

    fix next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,73 @@
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
 import PrismaDB from '@/libs/prismadb'
-import {compare} from "bcrypt"
-// @ts-ignore
-export default NextAuth({
-    providers: [
-        // Provide Credentials
-        CredentialsProvider({
-        id: 'credentials',
-            name: 'Credentials',
+import { compare } from "bcrypt"
+import Credentials from "next-auth/providers/credentials"
 
-            // Set the Credentials
-            credentials: {
-                email: {
-                    label: 'Email',
-                    type: 'text'
-                },
-                password: {
-                    label: 'Password',
-                    type: 'password'
-                },
-            },
 
-            // Make out authorize function
-            async authorize(credentials) {
-                if ( !credentials?.email || !credentials?.password ) {
-                    throw new Error('Email and password required')
-                }
 
-                // Try to find the user in DB
-                const user = await PrismaDB.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                })
+const handler = NextAuth({
+  providers: [
+    // Provide Credentials
+    Credentials({
+      id: 'credentials',
+      name: 'Credentials',
 
-                // If there is not a user or hashed password will throw an error
-                if (!user || !user.hashedPassword) {
-                    throw new Error("Email does not Exist")
-                }
-                // If there is a User
-                    // Compare the Password
-                const isCorrectPassword = await compare(credentials.password , user.hashedPassword)
+      // Set the Credentials
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text'
+        },
+        password: {
+          label: 'Password',
+          type: 'password'
+        },
+      },
 
-                    // Check if the Password is correct
-                if (!isCorrectPassword) {
-                    throw new Error("Password is not correct !")
-                }
-
-                return user
-            }
+      // Make out authorize function
+      async authorize(credentials) {
+        console.log('Stat Authorizing')
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password required')
+        }
+        console.log(credentials)
+        // Try to find the user in DB
+        const user = await PrismaDB.user.findUnique({
+          where: {
+            email: credentials.email
+          }
         })
-    ],
-    pages: {
-        signIn: '/login',
-    },
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: 'jwt'
-    },
-    secret: process.env.NEXTAUTH_SECRET
+
+        console.log(user)
+
+        // If there is not a user or hashed password will throw an error
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not Exist")
+        }
+        // If there is a User
+        // Compare the Password
+        const isCorrectPassword = await compare(credentials.password, user.hashedPassword)
+
+        // Check if the Password is correct
+        if (!isCorrectPassword) {
+          throw new Error("Password is not correct !")
+        }
+
+        return user
+      }
+    })
+  ],
+  pages: {
+    signIn: '/auth',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt'
+  },
+  secret: process.env.NEXTAUTH_SECRET
 })
+
+export {
+  handler as GET,
+  handler as POST,
+}
