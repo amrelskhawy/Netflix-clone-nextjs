a00236222ae2dc28c367be64a64744361d19f31e
amrelskhawy <amrelskhawy@gmail.com>
2024-02-21 21:41:19 +0200

    register and sign in now works

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,26 +1,53 @@
 import bcrypt from "bcrypt"
 import Prismadb from "@/libs/prismadb";
 import { NextApiResponse } from "next"
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
 
 export const GET = (req: Request) => {
 
-  return new Response('Bad Request', {status: 405})
+  return new Response('Bad Request', { status: 405 })
 }
 
 
-export const POST = async (req: Request) => {
+export const POST = async (req: Request, res: NextApiResponse) => {
   try {
-    // const {email, username, password} = req.body
-    // console.log({email, username, password} )
-    return new NextResponse('Hello Post', {
-      status: 200
+    const { email, name, password } = await req.json()
+    console.log({ email, name, password })
+
+    const existingUser = await Prismadb.user.findFirst({
+      where: {
+        email
+      }
     })
 
+    console.log(existingUser)
+
+    if (existingUser) {
+
+      return new Response('Email already taken', { status: 422 })
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    console.log(hashedPassword)
+
+    const user = await Prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: '',
+        emailVerified: new Date(),
+      }
+    })
+
+    console.log(user)
+
+    return new Response('Account has Been Created Successfully', { status: 201 })
+
   } catch (error: any) {
-
+    return new Response('Error Cause of -> ' + error, { status: 500 })
   }
-
-
 }
